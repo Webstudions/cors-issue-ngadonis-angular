89a1f50de2750d12304c048125a2a74aa75f376d
Nick Scheynen <nickscheynen@gmail.com>
2017-09-02 21:40:08 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http } from "@angular/http";
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public products:string[] = [];
+    
+  constructor(
+    public http: Http
+  ) {
+    this.http.get('http://localhost:3333/products')
+      .map(response => response.json())
+      .subscribe(data => this.products = data);
+
+  }
+
 }
